cabc0d020c0030257a2f82d6a09b62a0a14aa5be
disfrutoelmomento <148721887+disfrutoelmomento@users.noreply.github.com>
2023-12-10 23:06:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 import telebot
 from telebot import types
 import requests
@@ -6,9 +8,9 @@ import sqlite3
 import schedule
 import time
 import threading
+import json2html
 
 bot = telebot.TeleBot('6573749748:AAHkgu9YEZrSoO4azALlhsJBOTpuQaciMwA')
-
 
 # Функция для осуществления рассылки
 def newsletter():
@@ -85,8 +87,8 @@ def choose_option(message):
         bot.register_next_step_handler(message, choose_option)
     # Натальная карта
     elif message.text.lower() == '/natal_chart' or message.text.lower() == 'натальная карта' or message.text.lower() == 'natal chart':
-        bot.send_message(message.chat.id, 'Извини, эта функция появится позже')
-        bot.register_next_step_handler(message, choose_option)
+        bot.send_message(message.chat.id, 'Введите время в формате DD:MM:YYYY HH:MM и город')
+        bot.register_next_step_handler(message, natal_chart)
     # Совместимость
     elif message.text.lower() == 'совместимость' or message.text.lower() == '/compatibility' or message.text.lower() == 'compatibility':
         bot.send_message(message.chat.id, 'Введите два знака зодиака в формате: Рыбы Водолей')
@@ -108,9 +110,47 @@ def choose_option(message):
 
 # Функция для натальной карты
 def natal_chart(message):
-    pass
+    user_input = message.text.split()
 
-#парсинг совместимости
+    if len(user_input) < 3:
+        bot.reply_to(message, "Пожалуйста, укажите дату, время и место (город) в правильном формате.")
+        return
+
+    birth_date = user_input[0]  # Date format: DD:MM:YYYY
+    birth_time = user_input[1]  # Time format: HH:MM
+
+    parsed_date = datetime.datetime.strptime(birth_date + " " + birth_time, "%d:%m:%Y %H:%M")
+
+    # Создание нового объекта datetime с нужной датой (2000-01-01) и временем (00:00:00)
+    new_date = datetime.datetime(2000, 1, 1)
+    # Конечный формат даты и времени
+    formatted_date = new_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+    # Конечная точка API и параметры для создания натальной карты
+    api_endpoint = 'https://api.astroapi.cloud/horoscope/natal/calculate'
+    api_key = 'MndMdGc4by9RWlN1RVFlcSsrQXVDdz09Lit5c3lXbGdDVHF1aU1qalcxSFJLNmc9PQ=='
+
+    # Сделать запрос к API
+    response = requests.post(api_endpoint, json={"dateTime": formatted_date,
+                                                 "latitude": 50.8476424,
+                                                 "longitude": 4.3571696}, headers={'X-Api-Key': api_key})
+
+    if response.status_code == 200:
+        natal_chart = response.json()  # Предполагаем, что API возвращает данные в формате JSON
+        html = json2html.json2html.convert(json=natal_chart)
+        htmlFile = open("GFG-1.html", "w")
+        htmlFile.write(html)
+        htmlFile.close()
+
+        f = open("GFG-1.html", "rb")
+
+        bot.send_document(chat_id=message.chat.id, document=f)
+
+    else:
+        bot.reply_to(message, "Не удалось составить натальную карту. Пожалуйста, попробуйте снова.")
+
+
+# парсинг совместимости
 def compatibility_parcing(num):
     URL = 'https://www.nur.kz/esoterics/astrology/1824471-lubov-znakov-zodiaka-kak-lubit-kazdyj-znak/'
     r = requests.get(URL)
@@ -120,6 +160,7 @@ def compatibility_parcing(num):
     for i in range(1, len(collection), 2):
         merged_collection.append(collection[i - 1].text + collection[i].text)
     return f'{merged_collection[num]}'
+
 
 # Функция для совместимости
 def compatibility(message):
